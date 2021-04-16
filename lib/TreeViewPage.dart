f712dcae62ada12a40a9e4d2689473973bf7929e
Amr Abd-Alkrim <wizmoori@gmail.com>
2021-04-16 17:08:41 +0200

    clean and sample view with image

--- lib/TreeViewPage.dart
+++ lib/TreeViewPage.dart
@@ -12,104 +12,51 @@ class _TreeViewPageState extends State<TreeViewPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        Wrap(
-          children: [
-            Container(
-              width: 100,
-              child: TextFormField(
-                initialValue: builder.siblingSeparation.toString(),
-                decoration: InputDecoration(labelText: "Sibling Separation"),
-                onChanged: (text) {
-                  builder.siblingSeparation = int.tryParse(text) ?? 100;
-                  this.setState(() {});
-                },
-              ),
-            ),
-            Container(
-              width: 100,
-              child: TextFormField(
-                initialValue: builder.levelSeparation.toString(),
-                decoration: InputDecoration(labelText: "Level Separation"),
-                onChanged: (text) {
-                  builder.levelSeparation = int.tryParse(text) ?? 100;
-                  this.setState(() {});
-                },
-              ),
-            ),
-            Container(
-              width: 100,
-              child: TextFormField(
-                initialValue: builder.subtreeSeparation.toString(),
-                decoration: InputDecoration(labelText: "Subtree separation"),
-                onChanged: (text) {
-                  builder.subtreeSeparation = int.tryParse(text) ?? 100;
-                  this.setState(() {});
-                },
-              ),
-            ),
-            Container(
-              width: 100,
-              child: TextFormField(
-                initialValue: builder.orientation.toString(),
-                decoration: InputDecoration(labelText: "Orientation"),
-                onChanged: (text) {
-                  builder.orientation = int.tryParse(text) ?? 100;
-                  this.setState(() {});
-                },
-              ),
-            ),
-            RaisedButton(
-              onPressed: () {
-                final Node node12 = Node(getNodeText());
-                var edge = graph.getNodeAtPosition(r.nextInt(graph.nodeCount()));
-                print(edge);
-                graph.addEdge(edge, node12);
-                setState(() {});
-              },
-              child: Text("Add"),
-            )
-          ],
-        ),
-        Expanded(
-          child: InteractiveViewer(
-              constrained: false,
-              boundaryMargin: EdgeInsets.all(100),
-              minScale: 0.01,
-              maxScale: 5.6,
-              child: GraphView(
-                graph: graph,
-                algorithm: BuchheimWalkerAlgorithm(builder, TreeEdgeRenderer(builder)),
-                paint: Paint()
-                  ..color = Colors.green
-                  ..strokeWidth = 1
-                  ..style = PaintingStyle.stroke,
-              )),
-        ),
-      ],
+        body: Container(
+      color: Colors.black,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Expanded(
+            child: InteractiveViewer(
+                constrained: false,
+                boundaryMargin: EdgeInsets.all(100),
+                minScale: 0.01,
+                maxScale: 5.6,
+                child: GraphView(
+                  graph: graph,
+                  algorithm: BuchheimWalkerAlgorithm(
+                      builder, TreeEdgeRenderer(builder)),
+                  paint: Paint()
+                    ..color = Colors.green
+                    ..strokeWidth = 1
+                    ..style = PaintingStyle.stroke,
+                )),
+          ),
+        ],
+      ),
     ));
   }
 
-  Random r = Random();
-
-  int n = 1;
-
-  Widget getNodeText() {
+  Widget getNode() {
     return InkWell(
       onTap: () {
         print('clicked');
       },
       child: Container(
-          padding: EdgeInsets.all(16),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(4),
-            boxShadow: [
-              BoxShadow(color: Colors.blue[100], spreadRadius: 1),
-            ],
+          child: Column(
+        children: [
+          Image.asset(
+            "assets/img/3mrdev.png",
+            width: 100,
+            height: 100,
           ),
-          child: Text("Node ${n++}")),
+          Text(
+            "Amr Abd-Alkrim",
+            style: TextStyle(color: Colors.white),
+          )
+        ],
+      )),
     );
   }
 
@@ -118,29 +65,29 @@ class _TreeViewPageState extends State<TreeViewPage> {
 
   @override
   void initState() {
-    final Node node1 = Node(getNodeText());
-    final Node node2 = Node(getNodeText());
-    final Node node3 = Node(getNodeText());
-    final Node node4 = Node(getNodeText());
-    final Node node5 = Node(getNodeText());
-    final Node node6 = Node(getNodeText());
-    final Node node8 = Node(getNodeText());
-    final Node node7 = Node(getNodeText());
-    final Node node9 = Node(getNodeText());
-    final Node node10 = Node(getNodeText());
-    final Node node11 = Node(getNodeText());
-    final Node node12 = Node(getNodeText());
+    final Node node1 = Node(getNode());
+    final Node node2 = Node(getNode());
+    final Node node3 = Node(getNode());
+    final Node node4 = Node(getNode());
+    final Node node5 = Node(getNode());
+    final Node node6 = Node(getNode());
+    final Node node8 = Node(getNode());
+    final Node node7 = Node(getNode());
+    final Node node9 = Node(getNode());
+    final Node node10 = Node(getNode());
+    final Node node11 = Node(getNode());
+    final Node node12 = Node(getNode());
 
     graph.addEdge(node1, node2);
-    graph.addEdge(node1, node3, paint: Paint()..color = Colors.red);
-    graph.addEdge(node1, node4, paint: Paint()..color = Colors.blue);
+    graph.addEdge(node1, node3);
+    graph.addEdge(node1, node4);
     graph.addEdge(node2, node5);
     graph.addEdge(node2, node6);
-    graph.addEdge(node6, node7, paint: Paint()..color = Colors.red);
-    graph.addEdge(node6, node8, paint: Paint()..color = Colors.red);
+    graph.addEdge(node6, node7);
+    graph.addEdge(node6, node8);
     graph.addEdge(node4, node9);
-    graph.addEdge(node4, node10, paint: Paint()..color = Colors.black);
-    graph.addEdge(node4, node11, paint: Paint()..color = Colors.red);
+    graph.addEdge(node4, node10);
+    graph.addEdge(node4, node11);
     graph.addEdge(node11, node12);
 
     builder
